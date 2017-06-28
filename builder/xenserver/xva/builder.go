2710fec0513b402051bfb81b42193c3758cc55c6
soumyabk <soumyab@nvidia.com>
2017-06-28 14:53:26 -0700

    fix indentation error

--- builder/xenserver/xva/builder.go
+++ builder/xenserver/xva/builder.go
@@ -189,7 +189,7 @@ func (self *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (pa
 			VdiUuidKey: "tools_vdi_uuid",
 		},
 		new(xscommon.StepConfigureDiscDrives),
-		  &xscommon.StepExecuteHostScripts{
+		&xscommon.StepExecuteHostScripts{
                         ScriptType:   "post-stop",
                         LocalScripts: self.config.PostStopHostScripts,
                 },
